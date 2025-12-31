0129bb38cb11073107a4affcda1caada721146e9
Preetham K H <blighter.soul@gmail.com>
2025-12-31 16:46:51 +1100

    Add HttpResponse extensions for test assertions
    Introduced `HttpResponseExtensions` in the `PortfolioTracker.IntegrationTests.Helpers` namespace to simplify HTTP response handling in tests.

---
+++ PortfolioTracker.IntegrationTests/Helpers/HttpResponseExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace PortfolioTracker.IntegrationTests.Helpers
+{
+    /// <summary>
+    /// Extension methods for HttpResponse to simplify test assertions.
+    /// </summary>
+    public static class HttpResponseExtensions
+    {
+        /// <summary>
+        /// Checks if the HTTP response indicates a successful status code (2xx).
+        /// </summary>
+        /// <remarks>
+        /// Use with FluentAssertions like:
+        /// response.IsSuccessful().Should().BeTrue();
+        ///
+        /// Or:
+        /// response.Should().BeSuccessful(); // FluentAssertions has this built-in
+        /// </remarks>
+        public static bool IsSuccessful(this HttpResponseMessage response)
+        {
+            return response.IsSuccessStatusCode;
+        }
+
+        /// <summary>
+        /// Gets response status code as integer.
+        /// </summary>
+        public static int GetStatusCodeAsInt(this HttpResponseMessage response)
+        {
+            return (int)response.StatusCode;
+        }
+    }
+}
