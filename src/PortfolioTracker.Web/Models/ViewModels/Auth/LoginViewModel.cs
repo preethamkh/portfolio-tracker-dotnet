6eaee305a4ef0aee64bdf35ce21a593966c079eb
Preetham K H <blighter.soul@gmail.com>
2026-02-18 11:36:17 +1100

    Move LoginVM to Auth namespace

--- src/PortfolioTracker.Web/Models/ViewModels/Auth/LoginViewModel.cs
+++ src/PortfolioTracker.Web/Models/ViewModels/Auth/LoginViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace PortfolioTracker.Web.Models.ViewModels;
+namespace PortfolioTracker.Web.Models.ViewModels.Auth;
 
 public class LoginViewModel
 {
