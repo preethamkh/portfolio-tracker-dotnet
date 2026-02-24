129b27b3d91b9ad0e7d7b111d632373464ead72b
Preetham K H <blighter.soul@gmail.com>
2026-02-24 19:48:12 +1100

    WIP - AuthController

---
+++ src/PortfolioTracker.Web/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioTracker.Web.Interfaces.Services;
+
+namespace PortfolioTracker.Web.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly IApiClient _apiClient;
+    private readonly ITokenService _tokenService;
+
+    public AuthController(IApiClient apiClient, ITokenService tokenService)
+    {
+        _apiClient = apiClient;
+        _tokenService = tokenService;
+    }
+
+    // GET: /Auth/Login
+    [HttpGet]
+    public IActionResult Login(string? returnUrl = null)
+    {
+        // Already logged in? Redirect to dashboard
+        if (User.Identity?.IsAuthenticated == true)
+        {
+            return RedirectToAction("Index", "Dashboard");
+        }
+
+        return View(new Models.ViewModels.Auth.LoginViewModel { ReturnUrl = returnUrl });
+    }
+}
